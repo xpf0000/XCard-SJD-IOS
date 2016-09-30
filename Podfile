fba77a55780ece86812e3d4af3ffd1704fca9a2a
xpf0000 <xpf_163@163.com>
2016-09-30 11:42:38 +0800

    +++++

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform:ios, '8.0'
-target "CardSJD" do
-use_frameworks!
-pod 'SwiftCharts', '~> 0.4'
-end
-
--- Pods/Target Support Files/Pods-CardSJD/Pods-CardSJD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CardSJD : NSObject
-@end
-@implementation PodsDummy_Pods_CardSJD
-@end
